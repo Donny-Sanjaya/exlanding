7db35272d90977a5f431d0dfd07559ff86a08cb8
Donny-Sanjaya <miftahul.119130144@student.itera.ac.id>
2025-10-30 20:05:07 +0700

    finishing header dan hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,13 @@
 import 'boxicons/css/boxicons.min.css';
+import Spline from '@splinetool/react-spline';
 
 const Hero = () => {
   return (
     <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
         
-        <div className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0">
+        <div data-aos="fade-right"
+     data-aos-offset="300"
+     data-aos-easing="ease-in-sine" className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0">
             {/* tag box-with gradient border */}
             <div className='relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#656565] to-[#e99b63] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
                 <div className='absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1'>
@@ -34,9 +37,16 @@ const Hero = () => {
                 <a className='border border-[#2a2a2a] py-2 sm:py-3 px-8 sm:px-10 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#1a1a1a] bg-gray-300 text-black hover:text-white' href="#">
                     GetStarted <i class='bx  bx-link-external'  ></i> 
                 </a>
-            </div>
-
+            </div>  
         </div>
+
+        {/* 3D Robot */}
+        <Spline data-aos="fade-zoom-in"
+     data-aos-easing="ease-in-back"
+     data-aos-delay="300"
+     data-aos-offset="0" 
+     data-aos-duration="1500" 
+     className='absolute lg:top-0 top-[-20%] bottom-0 lg:left-[25%] sm:left-[-2%] h-full' scene="https://prod.spline.design/5xkYvM9Z4qbxR-9R/scene.splinecode" />
 
     </main>
   )
